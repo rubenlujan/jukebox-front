98941e2b3d05fadb100b0f38aba646c4fb6e92ef
Ruben Lujan <lujanortegaruben@gmail.com>
2026-05-08 17:54:55 -0600

    Cambios 2026-05-08

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './styles/index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ToastProvider>
-      <BrowserRouter basename="/rockola">
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </ToastProvider>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rockola/', // 👈 importantísimo
-  // base: '/',
+  //base: '/rockola/', // 👈 importantísimo
+  base: '/',
 })
